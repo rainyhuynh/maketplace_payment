33a4b4da49752f3e42e82acc8adf3f6bab176001
rainy huynh <rainyhuynh83@gmail.com>
2020-12-23 09:28:03 +0700

    add slug to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  authenticate :user, lambda { |u| u.admin? } do
-    mount Sidekiq::Web => '/sidekiq'
-  end
+  # authenticate :user, lambda { |u| u.admin? } do
+  #   mount Sidekiq::Web => '/sidekiq'
+  # end
+
+  mount Sidekiq::Web => '/sidekiq'
 
   resources :projects do
     resources :comments, module: :projects #make projects/comments#index
